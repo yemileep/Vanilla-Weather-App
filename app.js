49105e16da068796140dfb90de83c1da0e858664
yemileep <82692017+yemileep@users.noreply.github.com>
2022-01-07 18:50:35 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function formatDate(timestamp) {
-  let date = new Date(timestamps)
+  let date = new Date(timestamp)
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -25,23 +25,30 @@ function formatDate(timestamp) {
   let day = days[date.get()];
   return `${day} ${hours}:${minutes}`;
   
-}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature")
   let cityElement = document.querySelector("#city");
-    let descriptionElement = document.querySelector("#description");
+  let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon")
   temperatureElement.innerHTML = Math.round(respose.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(respose.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute(
+    "alt", response.data.weather[0].description);
 
-let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+}
+
+
+
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let city = "New York";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=NewYork&appid=${apiKey}&units=metric`;
 
